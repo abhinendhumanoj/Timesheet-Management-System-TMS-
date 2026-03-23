d48a612bb139e3e7d8ad6ef5cbaf2f3b45646026
Abhinendhu <abhinendhumanoj@gmail.com>
2026-03-23 13:59:19 +0530

    Prepare project for deployment

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const approvalRoutes = require('./routes/approvalRoutes');
 const reportRoutes = require('./routes/reportRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
